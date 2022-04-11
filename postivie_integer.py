3b8df0c468bb61a52336141d791c2434e67ebed3
Vamsi Chandra <72137172+VamsiVelamati@users.noreply.github.com>
2022-04-11 21:07:13 +0530

    Add files via upload

---
+++ postivie_integer.py
@@ -0,0 +1,12 @@
+list1=[12, -7, 5, 64, -14] 
+for numbers in list1:
+    if numbers>0:
+        print(numbers,end=" ")
+
+
+------------------------------------------------------------
+
+list2=[12, 14, -95, 3]
+for num in list2:
+    if num>0:
+        print(num,end=" ")
