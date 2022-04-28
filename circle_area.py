df7a43932a63688afa6ffc040b1db2b81164874a
Vamsi Chandra <72137172+VamsiVelamati@users.noreply.github.com>
2022-04-28 11:58:08 +0530

    Update circle_area.py

--- circle_area.py
+++ circle_area.py
@@ -1,5 +1,11 @@
+#radius of a circle
 radius = float(input(">: "))
 r=radius
 pie=22/7
 area = pie*(r**2)
 print(area)
+----------------------------------------------------------------------------------------------------------------------------
+#extension of the file
+name= input("Input the Filename: ")
+f= (name.split('.'))
+print("The extension of the file is: ",f[-1])
